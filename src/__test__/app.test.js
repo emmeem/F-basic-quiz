bfaa264f511b93d4eb5f66a977fdb0cb3681bc88
emmeem <1124782639@qq.com>
2020-09-15 18:54:22 +0800

    jest test

--- src/__test__/app.test.js
+++ src/__test__/app.test.js
@@ -1,3 +1,24 @@
-test("add 1+1 is 2", () => {
-  expect(2).toBe(2);
-});
+import {getUsers,getEducations} from '../request'
+
+jest.mock("../request")
+
+test("get users info", async () => {
+  const id = 1;
+  getUsers.mockImplementation(() => {});
+  getUsers(id);
+     
+  expect(getUsers).toHaveBeenCalled();
+  expect(getUsers).toHaveBeenCalledTimes(1);
+  });
+  
+
+  test("get educations", async () => {
+    const id = 1;
+    getEducations.mockImplementation(()=>{});
+    getEducations(id);
+    
+    expect(getEducations).toHaveBeenCalled();
+    expect(getEducations).toHaveBeenCalledTimes(1);
+  });
+  
+
--- src/app.js
+++ src/app.js
@@ -1,8 +1,7 @@
 import "./style/index.scss";
-
 import { getEducations, getUsers } from "./request";
 
-const path = window.location.pathname.split('/')
+const path = location.pathname.split('/')
 const id = path[2];
 getUsers(id);
 getEducations(id);
